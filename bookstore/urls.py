d1b5eebd24d2081037eb99af9eb228645ad491e4
arnavkosh <koshx42@gmail.com>
2025-09-06 16:24:34 +0530

    Initial commit

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -5,8 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),   # yaha namespace ki zaroorat nahi
-    path('cart/', include('cart.urls')),  # yaha bhi nahi
+    path('', include('shop.urls')),   
+    path('cart/', include('cart.urls')),
+path('orders/',
+include(('orders.urls', 'orders'),
+namespace='orders')),
 ]
 
 if settings.DEBUG:
--- cart/views.py
+++ cart/views.py
@@ -3,23 +3,26 @@ from django.views.decorators.http import require_POST
 from shop.models import Book
 from .cart import Cart
 
+
 @require_POST
 def cart_add(request, book_id):
     cart = Cart(request)
     book = get_object_or_404(Book, id=book_id)
     qty = int(request.POST.get('quantity', 1))
-    cart.add(book=book, quantity=qty)
+    cart.add(product=book, quantity=qty)
 
-    if request.POST.get("buy_now"):  # agar Buy Now button dabaya
-        return redirect("orders:order_create")  # seedha checkout pe bhej de
-    
+    if request.POST.get("buy_now"):
+        return redirect("orders:order_create")
+
     return redirect("cart:cart_detail")
+
 
 def cart_remove(request, book_id):
     cart = Cart(request)
     book = get_object_or_404(Book, id=book_id)
     cart.remove(book)
     return redirect("cart:cart_detail")
+
 
 def cart_detail(request):
     cart = Cart(request)
